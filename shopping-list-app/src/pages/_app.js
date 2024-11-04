d442c8409d1c1b953b6a3b92092792de65b9a629
Deny0412 <denygrisa@gmail.com>
2024-11-04 20:36:14 +0100

    update

--- shopping-list-app/src/pages/_app.js
+++ shopping-list-app/src/pages/_app.js
@@ -2,13 +2,16 @@
 import ShoppingListProvider from "../contexts/ShoppingListContext"; // Default import
 import Layout from "../components/Layout";
 import "../globals.css";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ShoppingListProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ShoppingListProvider>
+    <ThemeProvider>
+      <ShoppingListProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ShoppingListProvider>
+    </ThemeProvider>
   );
 }
